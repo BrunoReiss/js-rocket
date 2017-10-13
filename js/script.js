d005233d4453574229236c0bc6f8dc69d170ac95
BrunoReiss <brunoreis@mail.com>
2017-10-13 04:35:47 -0300

    Made some car stuff happen

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var sayMyName = function (name) {
+	alert("My name is: " + name)
+};
+
+var car = {
+			make: "VW",
+			type: "Polo",
+			colour: "blue",
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: [
+				"seat1",
+			 	"seat2", 
+			 	"seat3", 
+			 	"seat4"
+			 	],
+			 turnOn: function(){
+			 	this.isTurnedOn = true; //"This" refers to the car object here
+			 	},
+			 fly: function(){
+			 		alert("flying")
+			 	},
+			 switchCar: function(isOn){
+			 	console.log("turn car" + " " + isOn);
+			 		if(isOn == true){
+			 			this.isTurnedOn = true
+			 		}
+			 		else{
+			 			this.isTurnedOn = false
+			 		}
+			 	}
+		};
+		
+			console.log("hello there friends!")
